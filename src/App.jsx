2e5ea9355626b48ae727347e4fc5911380f2e179
migdalius <nyjepl@gmail.com>
2023-05-09 09:17:56 +0200

    Static Navbar

---
+++ src/App.jsx
@@ -0,0 +1,167 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
+        vitae dolorum. Aliquid consectetur, minus ratione laudantium atque odit
+        cumque, illo accusantium, consequuntur distinctio sit dolorum qui eum
+        quasi? Sit, itaque!
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Wojciech Nowak",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">Herkis</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Link 1</span>
+          <span>Link 2</span>
+          <span>Link 3</span>
+          <span>Link 4</span>
+          <span>Link 5</span>
+          {!currentUser?.isSeller && <button>Dodaj Piekarnie</button>}
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test 1</span>
+            <span> Test 2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
